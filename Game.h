8093f9aa44c4fc334c369f45b1b5ed454f46bf0e
Alex Simpson <143616965+asimp24@users.noreply.github.com>
2024-04-05 09:33:45 -0400

    Add files via upload
    All the object .h files we should need

---
+++ Game.h
@@ -0,0 +1,8 @@
+//
+// Created by administrator on 4/5/24.
+//
+
+#ifndef FINALSEM2_GAME_H
+#define FINALSEM2_GAME_H
+
+#endif //FINALSEM2_GAME_H
---
+++ Mode.h
@@ -0,0 +1,8 @@
+//
+// Created by administrator on 4/5/24.
+//
+
+#ifndef FINALSEM2_MODE_H
+#define FINALSEM2_MODE_H
+
+#endif //FINALSEM2_MODE_H
